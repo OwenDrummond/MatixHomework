b9e048c3d801572721c4dad8b00465088978072c
OwenDrummond <129247532+OwenDrummond@users.noreply.github.com>
2023-09-22 10:40:50 -0500

    Update Matrix.java

--- Matrix.java
+++ Matrix.java
@@ -5,11 +5,14 @@ public class Matrix
    public static void main(String [] args)
    {
        Matrix newArray = new Matrix();
+      // Creation of 2D array
        int[][] arr = {{1,2,3,4}, {5,6,7,8}, {9,10,11,12}};
+      // Calling Matrix Search with a number we are seraching for
        int[] answer = MatrixSearch(arr, 10); 
    }
    static int[] MatrixSearch(int[][] arr, int key)
    {
+      // Using row and column counts to create the max size of array and declaring a 0 for minimum size of array.
        int rowCount = arr.length;
        int colCount = arr[0].length;
        int lowBound = 0;
@@ -17,6 +20,7 @@ public class Matrix
        
        while(lowBound <= highBound)
        {
+          // We start search from middle of array.
            int middle = (highBound - 1) + lowBound / 2;
            int currRow = middle % colCount;
            int currCol = middle / colCount;
@@ -24,17 +28,21 @@ public class Matrix
            
            if(currSearch == key)
            {
+              // If the middle "coordinates" line up to the key value, it returns that spot in the 2D array. 
                return new int[] { currRow, currCol };
            }
            if(currSearch < key)
            {
+              // We then search the right side from midddle if the key is more than our intial search.
                lowBound = middle + 1;
            }
            else
            {
+              // If the key is lower than our initial search we search the left side of the array. 
                highBound = middle -1;
            }
        }
+      // If we do not find the value in the array, we return a negative array value to indicate it is not there. 
        return new int[] {-1, -1}; 
    }
 }
